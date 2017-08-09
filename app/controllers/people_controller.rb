68f33b0cf49b03020d8ad7dcd3fbdd77dff5409d
ShunInoue <shuinoue@pasonatech.co.jp>
2017-08-09 03:34:40 +0000

    create person

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,29 @@
+class PeopleController < ApplicationController
+  def index
+    @msg = 'Person data.'
+    @data = Person.all
+  end
+  
+  def show 
+    @msg = 'Person data.'
+    @data = Person.find(params[:id]) 
+  end
+  
+  def add
+    	@msg = "add new data."
+    	@person = Person.new
+  end
+
+def create
+	if request.post? then
+		Person.create(person_params)
+	end
+	redirect_to '/people'
+end
+
+private
+def person_params
+  params.require(:person).permit(:name, :age, :mail)
+end 
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'people/index'
+  get 'people/add'
+  get 'people', to: 'people#index'
+  get 'people/:id', to: 'people#show'
+  post 'people/add', to: 'people#create'
+
 get 'dengonban', to: 'dengonban#index'
 post 'dengonban', to: 'dengonban#index'
 get 'dengonban/index'
