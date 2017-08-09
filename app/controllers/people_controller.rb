fd78a2825f49e1380a2c635140c964c3cb5846a0
ShunInoue <shuinoue@pasonatech.co.jp>
2017-08-09 04:31:44 +0000

    use CRUD

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -21,9 +21,27 @@ def create
 	redirect_to '/people'
 end
 
+def edit
+  @msg = "edit data.[id = "+ params[:id] + "]"
+  @person = Person.find(params[:id])
+end
+
+def update
+  obj = Person.find(params[:id])
+  obj.update(person_params)
+  redirect_to '/people'
+end
+
+def delete
+  obj = Person.find(params[:id])
+  obj.destroy
+  redirect_to '/people'
+end
+
 private
 def person_params
   params.require(:person).permit(:name, :age, :mail)
+  #require・・・()のなかの値があるかどうかチェックしている
 end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
   get 'people', to: 'people#index'
   get 'people/:id', to: 'people#show'
   post 'people/add', to: 'people#create'
+  get 'people/edit/:id', to: 'people#edit'
+  patch 'people/edit/:id', to:'people#update'
+  get 'people/delete/:id', to: 'people#delete'
 
 get 'dengonban', to: 'dengonban#index'
 post 'dengonban', to: 'dengonban#index'
 get 'dengonban/index'
 post 'dengonban/index'
+
 end
