f77001a6e2851d3f0a12a3129771d1b40ea0f3b6
ShunInoue <shuinoue@pasonatech.co.jp>
2017-08-16 00:17:29 +0000

    create blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,55 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+	get 'blogs', to: 'blogs#index'
+	get 'blogs/:page', to: 'blogs#index'
+	
+	
+  get 'blogs/genre/:id', to: 'blogs#genre'
+  get 'blogs/genre/:id/:page', to: 'blogs#genre'
+
+  get 'blogs/show/:id', to: 'blogs#show'
+
   get 'blogposts/index'
+  get 'blogposts', to: 'blogposts#index'
+  post 'blogposts/delete/:id', to: 'blogposts#delete'
 
   get 'blogposts/add'
+  post 'blogposts/add'
 
-  get 'blogposts/edit'
+  get 'blogposts/:id', to: 'blogposts#edit'
+  patch 'blogposts/:id', to: 'blogposts#edit'
 
   get 'blogposts/delete'
 
   get 'bloggenres/index'
+  get 'bloggenres', to: 'bloggenres#edit'
 
   get 'bloggenres/add'
+  post 'bloggenres/add'
 
-  get 'bloggenres/edit'
+  get 'bloggenres/:id', to: 'bloggenres#edit'
+  patch 'bloggenres/:id', to: 'bloggenres#edit'
 
   get 'blogconfigs/index'
+  get 'blogconfigs', to: 'blogconfigs#index'
 
   get 'blogconfigs/edit'
+  patch 'blogconfigs/edit'
 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   get 'messages/index'
   get 'messages', to: 'messages#index'
 
